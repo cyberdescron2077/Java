e800375d555e6a531f2db7ca2c5abe73807a5a6c
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-27 19:17:17 +0300

    Create TaxCalculator.java

---
+++ 1-10/TaxCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TaxCalculator {
+    public static void main(String[] args){
+        double price,tax,total;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the price of the product :");
+        price = scanner.nextDouble();
+        System.out.println("Enter tax rate");
+        tax = scanner.nextDouble();
+        total = price + (price * tax/100);
+
+        System.out.println("You total would be : " + total);
+    }
+}
