2bcff99a58201b84ab641f9bdc78cac6f385795b
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-27 17:57:56 +0300

    Update Variables.java

---
+++ 1-10/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args){
+        
+        int num1 = 25;
+        double num2 = 25.5;
+        float num3 = 25.25f;
+        byte num4 = 127;
+        long num5 = 255;
+        boolean isTrue = true;
+        String sayHello = "hello";
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(isTrue);
+        System.out.println(sayHello);
+        
+
+    }
+}
