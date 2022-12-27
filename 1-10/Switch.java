7ee9144d39ca1e626ddc440e0d166a7c4a87e460
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-27 19:49:31 +0300

    Update Switch.java

--- 1-10/Switch.java
+++ 1-10/Switch.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Switch {
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int choice = scanner.nextInt();
+
+        switch(choice){
+            case 1: System.out.println("You have choosen 1");
+            break;
+            case 2: System.out.println("You have choosen 2");
+            break;
+            case 3: System.out.println("You have choosen 3");
+            break;
+            default: System.out.println("Enter a number between 1-3");
+            break;
+        }
     }
 }
