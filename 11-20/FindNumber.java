1d7c3da7dc4725dcb87cebc2c96eb009dd264346
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-28 01:45:03 +0300

    Create FindNumber.java

---
+++ 11-20/FindNumber.java
@@ -0,0 +1,19 @@
+public class FindNumber {
+    public static void main(String[] args) {
+        System.out.println("Program to search for a specific number in an array");
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+		int target = 5;
+		boolean numExist = false;
+		
+		for(int num : nums) {
+			if(num == target) {
+				numExist = true;
+				break;
+			}else {
+				numExist = false;
+			}
+		}
+		System.out.println("Does number exist? " + numExist );
+        
+    }
+}
