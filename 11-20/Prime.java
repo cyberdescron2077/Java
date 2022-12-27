55c4cdfc818a9c9130c6acc194766ccece2f17e7
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-28 01:42:29 +0300

    Create Prime.java

---
+++ 11-20/Prime.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Program to check if a number is prime or not");
+	    System.out.println("Enter a number to check prime or not");
+	    int choice = scanner.nextInt();
+	    
+	    if(choice<=1) {
+	    	System.out.println("Not a prime");
+	    	return;
+	    }
+	    if(choice % 2 == 0) {
+	    	if(choice == 2) {
+	    		System.out.println("Prime");
+	    	}
+	    	else {
+	    		System.out.println("Not a prime");
+	    	}
+	    }
+	    else {
+	    	System.out.println("Prime");
+	    }
+	    scanner.close();
+    }
+}
