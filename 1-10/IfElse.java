5c8a8b0271ec9e305239b2a45c0450b0148d7a9b
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-27 19:47:07 +0300

    Correcting changes

--- 1-10/IfElse.java
+++ 1-10/IfElse.java
@@ -3,11 +3,13 @@ public class IfElse {
         int num1 = 10;
         int num2 = 20;
         int num3 = 30;
-
-        if(a>b && a>c){
+        
+        if(num1>num2 && num1>num2){
             System.out.println(num1 + " is the biggest number");
-        }else if(b>a && b>c){
+        }else if(num2>num1 && num2>num3){
             System.out.println(num2 + " is the biggest number");
+        }else{
+            System.out.println(num3 + " is the biggest number");
         } 
     }
 }
