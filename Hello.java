63eb295c15f5032ab89f25e6bf272c7633280913
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-27 16:26:04 +0300

    Create Hello.java
    first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		System.out.println(15 + 10);
+		System.out.print("Hello World!\n");
+		System.out.println("15" + 10);
+		System.out.println("Hello,\tWorld!");
+		System.out.println("Hello \\ World!");
+	}
+
+}
