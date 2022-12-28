8e46b561bd863f29f0c483fd92b519782d1dfe55
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-29 02:21:51 +0300

    OOP

--- J31/J02/EmpDriver.java
+++ J31/J02/EmpDriver.java
@@ -1,5 +1,15 @@
 package J31.J02;
 
 public class EmpDriver {
-    
+    public static void main(String[] args){
+
+        Employee employee = new Employee("Tunc",1000, 45, 2015);
+       
+        employee.toString(employee);
+
+        Employee employee2 = new Employee("Kagan", 1500, 46, 2010);
+
+        employee2.toString(employee2);
+       
+    }
 }
--- J31/J02/Employee.java
+++ J31/J02/Employee.java
@@ -1,7 +1,46 @@
 package J31.J02;
 
 public class Employee {
-    public static void main(String[] args){
-        
+    private String empName;
+    public double empSalary;
+    private int empWorkHours;
+    private int empHireYear;
+    Employee(String empName,double empSalary,int empWorkHours,int empHireYear){
+        this.empName = empName;
+        this.empSalary = empSalary;
+        this.empWorkHours = empWorkHours;
+        this.empHireYear = empHireYear;
+    }
+    public double tax(double empSalary){
+        if(this.empSalary>=1000){
+            return (this.empSalary * 0.03);
+        }
+        return 0.0;
+    }
+    public int bonus(int empWorkHours){
+        int bonus = this.empWorkHours - 45;
+        if(bonus >= 0){
+            return bonus * 30;
+        }
+        return 0;
+    }
+
+    public double raiseSalary(int empHireYear){
+        int workYear = 2022 - this.empHireYear;
+        if(workYear < 10){
+            return this.empSalary * 0.05;
+        }else if(workYear >=10 && workYear <=20){
+            return this.empSalary * 0.10;
+        }
+        return this.empSalary * 0.15;
+    }
+
+    public void toString(Employee employee){
+        System.out.println("Name : " + this.empName);
+        System.out.println("Tax : " + this.tax(empSalary));
+        System.out.println("Bonus : " + this.bonus(60));
+        System.out.println("Salary raise : " + this.raiseSalary(2015));
+        double totalSalary = this.empSalary - this.tax(empSalary) + this.raiseSalary(empHireYear) + this.bonus(empWorkHours) ;
+        System.out.println("Your total salary is : " + totalSalary);
     }
 }
