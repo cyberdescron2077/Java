c666799cc3c822363d9ffbb5407c5597afea16a9
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-28 22:59:16 +0300

    define constructor

--- J31/Calculator.java
+++ J31/Calculator.java
@@ -3,4 +3,24 @@ package J31;
 public class Calculator {
     public int number1;
     public int number2;
+    public String color;
+    Calculator(int number1,int number2,String color){
+        this.number1 = number1;
+        this.number2 = number2;
+        this.color = color;
+
+    }
+    public int add(){
+        return number1 + number2;
+    }
+    public int sub(){
+        return number1 - number2;
+    }
+    public int mul(){
+        return number1 * number2;
+    }
+    public int div(){
+        return number1 / number2;
+    }
+
 }
--- J31/Oop.java
+++ J31/Oop.java
@@ -3,16 +3,18 @@ package J31;
 
 public class Oop {
     public static void main(String[] args){
-        Calculator calculator = new Calculator();
-        calculator.number1 = 10;
-        calculator.number2 = 20;
+        Calculator calculator = new Calculator(10,5,"Blue");
+
         System.out.println(calculator.number1);
         System.out.println(calculator.number2);
-        Calculator calculator2 = new Calculator();
-        calculator2.number1 = 20;
-        calculator2.number2 = 10;
+        System.out.println(calculator.add());
+        
+        Calculator calculator2 = new Calculator(1,2,"Black");
+        
         System.out.println(calculator2.number1);
         System.out.println(calculator2.number2);
+
+        System.out.println(calculator2.div());
     }
     
 }
