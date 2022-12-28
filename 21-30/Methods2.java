13d1fb04fa71d4058a8e5af19f094fda4b4fefba
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-28 18:55:40 +0300

    Create Methods2.java

---
+++ 21-30/Methods2.java
@@ -0,0 +1,28 @@
+public class Methods2 {
+    public static void main(String[] args){
+
+    // return
+    int a = power(4,2);
+    System.out.println(a);
+    
+    //void
+    power2(4,2);
+
+    }
+
+    static int power(int a,int b){
+        int result = 1;
+        for(int i=1; i<=b; i++){
+            result *= a;
+        }
+        return result;
+    }
+
+    static void power2(int a,int b){
+        int result = 1;
+        for(int i=1; i<=b; i++){
+            result *= a;
+        }
+        System.out.println(result);
+    }
+}
