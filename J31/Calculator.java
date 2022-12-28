662e9b60c0b73d0b87aa676ebac65740e5df59a0
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-28 21:58:52 +0300

    Learning OOP
    Here i call variables from another class

---
+++ J31/Calculator.java
@@ -0,0 +1,6 @@
+package J31;
+
+public class Calculator {
+    public int number1;
+    public int number2;
+}
---
+++ J31/Oop.java
@@ -0,0 +1,14 @@
+package J31;
+import J31.Calculator;
+
+public class Oop {
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        calculator.number1 = 10;
+        calculator.number2 = 20;
+
+        System.out.println(calculator.number1);
+        System.out.println(calculator.number2);
+    }
+    
+}
