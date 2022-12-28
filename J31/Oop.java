901b3546cdb6aaf0e18012c06fde7fc79064618c
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-28 22:12:02 +0300

    Update Oop.java

--- J31/Oop.java
+++ J31/Oop.java
@@ -8,6 +8,11 @@ public class Oop {
         calculator.number2 = 20;
         System.out.println(calculator.number1);
         System.out.println(calculator.number2);
+        Calculator calculator2 = new Calculator();
+        calculator2.number1 = 20;
+        calculator2.number2 = 10;
+        System.out.println(calculator2.number1);
+        System.out.println(calculator2.number2);
     }
     
 }
