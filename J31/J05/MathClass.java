9430bc1df58d2a297e00844f935d665dfbd5bb24
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-31 10:30:34 +0300

    Create MathClass.java

---
+++ J31/J05/MathClass.java
@@ -0,0 +1,18 @@
+package J31.J05;
+import java.lang.Math;
+public class MathClass {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+
+        System.out.println(Math.min(a, b));
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.abs(a));
+        System.out.println(Math.abs(b));
+        System.out.println(Math.pow(a, b));
+        System.out.println(Math.addExact(a, b));
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.random());
+        System.out.println(Math.sqrt(a));
+    }
+}
