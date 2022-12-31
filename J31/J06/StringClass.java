c281b432d62089e427077b93399be749f302e7cb
Tunç Öncü <cyberdescron2077@gmail.com>
2022-12-31 10:41:25 +0300

    Create StringClass.java

---
+++ J31/J06/StringClass.java
@@ -0,0 +1,18 @@
+
+public class StringClass{
+    public static void main(String[] args) {
+        String name = "Tunc Oncu";
+        
+        System.out.println(name.length());
+        System.out.println(name.concat(" Welcome"));
+        System.out.println(name.indexOf("u"));
+        System.out.println(name.charAt(5));
+        System.out.println(name.compareTo("Tunc OncU"));
+        System.out.println(name.compareToIgnoreCase("TUNC ONCU"));
+        System.out.println(name.contains("Oncu"));
+        System.out.println(name.endsWith("cu"));
+        System.out.println(name.replaceFirst("Tunc", "Oncu"));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+    }
+}
