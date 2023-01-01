6b0152535f9b2a516d698bd78d565a2b3947d330
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-01 22:02:46 +0300

    Update Main.java

--- J31/J10/Main.java
+++ J31/J10/Main.java
@@ -1,21 +1,23 @@
 package J31.J10;
 import java.util.Scanner;
-import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+  
 
         int row,column;
 
-        //System.out.println("Welcome to the Mine Field game");
-        //System.out.println("Enter dimensions of the board");
-        //System.out.println("Enter Rows :");
-        //row = scanner.nextInt();
-        //System.out.println("Enter Columns :");
-        //column = scanner.nextInt();
-        MineField mineField = new MineField(2,2);
+        System.out.println("Welcome to the Mine Field game");
+        System.out.println("Enter dimensions of the board");
+        System.out.println("Enter Rows :");
+        row = scanner.nextInt();
+        System.out.println("Enter Columns :");
+        column = scanner.nextInt();
+        
+        MineField mineField = new MineField(row,column);
         
         mineField.run();
+        scanner.close();
     }
 }
