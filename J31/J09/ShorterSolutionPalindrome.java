86123e282012c7507bc4c3b66f2b27dd83f933d6
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-01 17:50:00 +0300

    Create ShorterSolutionPalindrome.java

---
+++ J31/J09/ShorterSolutionPalindrome.java
@@ -0,0 +1,11 @@
+package J31.J09;
+
+public class ShorterSolutionPalindrome {
+    public static void main(String[] args) {
+        String str = "alla";
+        String temp = "";
+        for(int i=str.length()-1;i>=0;i--){
+            temp += str.charAt(i);
+        }
+    }
+}
