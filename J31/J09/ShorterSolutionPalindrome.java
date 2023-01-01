f02b786850ddd218e7ba89d3f6e7500c610f9122
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-01 17:50:57 +0300

    Update ShorterSolutionPalindrome.java

--- J31/J09/ShorterSolutionPalindrome.java
+++ J31/J09/ShorterSolutionPalindrome.java
@@ -7,5 +7,8 @@ public class ShorterSolutionPalindrome {
         for(int i=str.length()-1;i>=0;i--){
             temp += str.charAt(i);
         }
+        if(str.equals(temp)){
+            System.out.println("Palindrome!");
+        }
     }
 }
