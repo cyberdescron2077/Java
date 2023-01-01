4c0ac1f824de6f9c0cae697eeb5c82eb1a110a59
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-02 01:22:13 +0300

    firstcommit

---
+++ J41/AccessModifiers.java
@@ -0,0 +1,24 @@
+package J41;
+
+public class AccessModifiers {
+    private int enginePower,model;
+    public int speed;
+    protected String color;
+
+    AccessModifiers(){
+        this.model = 2023;
+        this.enginePower = 300;
+        this.speed = 120;
+        this.color = "Blue";
+        
+    }
+    private void print(){
+        System.out.println(model);
+        System.out.println(enginePower);
+        System.out.println(speed);
+        System.out.println(color);
+    }
+    protected void run(){
+        print();
+    }
+}
---
+++ J41/Main.java
@@ -0,0 +1,9 @@
+package J41;
+
+public class Main {
+    public static void main(String[] args) {
+        AccessModifiers accessModifiers = new AccessModifiers();
+
+        accessModifiers.run();
+    }
+}
