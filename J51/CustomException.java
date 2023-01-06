517c3c17f53d461ef3fcfc94639ea90df99bb2c9
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-07 00:35:46 +0300

    Custom exception throw keyword

---
+++ J51/CustomException.java
@@ -0,0 +1,11 @@
+public class CustomException extends Exception{
+   
+    public CustomException(){
+
+    }
+
+    public CustomException(String msg){
+        super(msg);
+    }
+    
+}
--- J51/Main.java
+++ J51/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int i = 0;
+
+        try {
+            if(i == 0){
+                throw new CustomException();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Try using integer other than 0!!");
+        }
+
+
+
+
     }
 
 }
