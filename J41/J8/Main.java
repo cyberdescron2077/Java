4b9adb8d513d84aeea91adbe669c02b2bd5bc47b
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-06 03:50:49 +0300

    Abstraction
    Data hiding

--- J41/J8/Main.java
+++ J41/J8/Main.java
@@ -3,5 +3,23 @@ package J41.J8;
 public class Main {
     public static void main(String[] args) {
         //Abstraction 
+        Pig pig = new Pig();
+        pig.animalSound();
+        pig.sleep();
     }
+
 }
+
+    abstract class Animal{
+        public abstract void animalSound();
+
+        public void sleep(){
+            System.out.println("Zzz Zzz");
+        }
+    }
+
+    class Pig extends Animal{
+        public void animalSound(){
+            System.out.println("Pig says : Wee Wee");
+        }
+    }
