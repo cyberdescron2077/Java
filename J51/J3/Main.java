f568fedf531257efae6988d40a29e22a0f7487b6
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-16 08:27:58 +0300

    mysql

---
+++ J51/J3/Main.java
@@ -0,0 +1,11 @@
+package J3;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        
+    }
+}
