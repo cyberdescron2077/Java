68978be2ef07eb722e1ca9ce5af76504f57d577a
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-24 19:56:24 +0300

    HashSet

---
+++ J51/J4/Main.java
@@ -0,0 +1,25 @@
+package J4;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet h = new HashSet();
+        h.add(123);
+        h.add(231);
+        h.add("tunc");
+        h.add("a");
+        h.add(true);
+        h.add(null);
+        h.remove(231);
+        h.forEach(System.out::println);
+
+        System.out.println("*************************");
+        Iterator itr = h.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+        
+    }
+}
