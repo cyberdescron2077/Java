cdd8f6c5bf1468d493fd37886202b5eb4666b405
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-25 15:20:59 +0300

    Priority Queue

--- J61/J2/Main.java
+++ J61/J2/Main.java
@@ -1,5 +1,19 @@
 package J61.J2;
 
+import java.util.PriorityQueue;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        PriorityQueue q = new PriorityQueue();
+
+        q.add(80);
+        q.add(20);
+        q.add(50);
+        q.add(10);
+
+        while(!q.isEmpty()){
+            System.out.println(q.poll());
+        }
+
+    }
 }
