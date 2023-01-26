885bd3013a66b9af1df3cad09728c68eba20f69b
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-26 14:39:28 +0300

    LinkedHashMap

---
+++ J61/J4/Main.java
@@ -0,0 +1,18 @@
+package J61.J4;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedHashMap l = new LinkedHashMap();
+        l.put("Jason", 90);
+        l.put("David", 80);
+        l.put("Tim", 70);
+
+        System.out.println(l);
+
+    }
+}
