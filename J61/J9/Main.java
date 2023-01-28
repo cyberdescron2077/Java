be3e7a2d326a6210ecfb95ce590d66f8ed09ee5f
Tunç Öncü <cyberdescron2077@gmail.com>
2023-01-28 14:10:15 +0300

    Button action

--- J61/J9/Main.java
+++ J61/J9/Main.java
@@ -1,5 +1,8 @@
 package J61.J9;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,10 +14,19 @@ public class Main {
         JFrame frame = new JFrame("Button example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JButton btn = new JButton("Button");
-        btn.setBounds(0,0,100,30);
+        btn.setBounds(100,100,100,30);
+        btn.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                System.out.println("Button has been clicked");
+            }
+            
+        });
+        
+        
         frame.add(btn);
-        
-        
         frame.setSize(600,600);
         frame.setLayout(null);
         frame.setVisible(true);
